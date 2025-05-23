960bc43fd3a257b83a13eba280a0deef74ef971b
unknown <nadigersankalp@gmail.com>
2025-05-23 20:02:25 +0530

    swagger title update

--- backend/swagger.js
+++ backend/swagger.js
@@ -6,9 +6,9 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'FitFull API',
+      title: 'CareFull API',
       version: '1.0.0',
-      description: 'API documentation for FitFull healthcare platform',
+      description: 'API documentation for CareFull healthcare platform- Medical E-commerce and pharmacy management system',
     },
     servers: [{ url: 'http://localhost:8000' }],
     components: {
